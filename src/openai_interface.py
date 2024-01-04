dc6c65fd776794bf91e7a0b6664e0b1eaac3f104
Artur Yasnov <65731060+ArturYasnov@users.noreply.github.com>
2024-01-04 21:15:04 +0100

    Update openai_interface.py

--- src/openai_interface.py
+++ src/openai_interface.py
@@ -9,12 +9,12 @@ def log_conversation(question, response):
                 file.write("\n")  # Adds a newline for readability between conversations
 
 
-def get_openai_response(input_text, system_prompt):
+def get_openai_response_login(input_text, system_prompt, user_info, recommended_content):
     print("Request has been sent.")
 
     client = OpenAI()
-    user_info = read_user_info()
-    recommended_content = extract_recommended_to_user_content(read_user_history()) 
+    # user_info = read_user_info()
+    # recommended_content = extract_recommended_to_user_content(read_user_history()) 
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",  # "gpt-3.5-turbo", "gpt-4-1106-preview"
@@ -28,5 +28,23 @@ def get_openai_response(input_text, system_prompt):
 
     # Log the conversation
     log_conversation(input_text, response_text)
+
+    return response_text
+
+
+def get_openai_response_logout(input_text, system_prompt, user_info):
+    print("Request has been sent.")
+
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",  # "gpt-3.5-turbo", "gpt-4-1106-preview"
+        messages=[
+            {"role": "system", "content": f"{system_prompt} Questions go in format User profile: ..., User question ..."},
+            {"role": "user", "content": f"User profile: {user_info}. User question: {input_text}."}
+        ]
+    )
+
+    response_text = completion.choices[0].message.content
 
     return response_text
